72d9f060dba3396b5b33788d6ebdfe3e15304eea
arthur gumery arthur.gumery5@gmail.com <arthur.gumery5@gmail.com>
2023-11-28 14:14:45 +0100

    event_places

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :event_users, dependent: :destroy
 
-  validates :first_name, :last_name, :nickname, :email, :password, :latitude, :longitude, :address, presence: true
+  validates :first_name, :last_name, :nickname, :email, :password, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
---
+++ test/controllers/event_places_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EventPlacesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
